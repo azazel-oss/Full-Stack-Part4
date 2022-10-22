1ba015cd9e7df8293cf9c5f89572b20060836f1a
Asad Mahmood <asadmhmd1234@gmail.com>
2022-10-22 15:23:23 +0530

    Finish exercise 5

--- tests/blogHelpers.test.js
+++ tests/blogHelpers.test.js
@@ -61,3 +61,66 @@ describe('total likes', () => {
 		expect(result).toBe(81)
 	})
 })
+
+describe('favourite blogpost', () => {
+	const listWithOneBlog = [
+		{
+			_id: '5a422aa71b54a676234d17f8',
+			title: 'Go To Statement Considered Harmful',
+			author: 'Edsger W. Dijkstra',
+			url: 'www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 5,
+			__v: 0
+		}]
+	const listOfManyBlogs = [
+		{
+			_id: ("6353888627d65a7de1f6946f"),
+			title: 'This is a title',
+			author: 'Asad Mahmood',
+			url: 'www.hoops.com',
+			likes: 10,
+			__v: 0
+		},
+		{
+			_id: ("6353b0e95f0d2db327197e4d"),
+			title: 'Another title',
+			author: 'Asad Mahmood',
+			url: 'www.hoops.com',
+			likes: 23,
+			__v: 0
+		},
+		{
+			_id: ("6353b1005f0d2db327197e4f"),
+			title: 'one more title',
+			author: 'asad mahmood',
+			url: 'www.hoops.com',
+			likes: 48,
+			__v: 0
+		}
+	]
+
+	test('if single blog post is sent, equals that post', () => {
+		const result = listHelper.favoriteBlog(listWithOneBlog)
+		expect(result).toEqual({
+			_id: '5a422aa71b54a676234d17f8',
+			title: 'Go To Statement Considered Harmful',
+			author: 'Edsger W. Dijkstra',
+			url: 'www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 5,
+			__v: 0
+		})
+	})
+
+	test('if multiple blogs are sent, equals most liked post', () => {
+		const result = listHelper.favoriteBlog(listOfManyBlogs)
+		expect(result).toEqual({
+				_id: ("6353b1005f0d2db327197e4f"),
+				title: 'one more title',
+				author: 'asad mahmood',
+				url: 'www.hoops.com',
+				likes: 48,
+				__v: 0
+			}
+		)
+	})
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = (blogs) => {
 	return blogs.reduce((acc, blog) => acc + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+	return blogs.reduce((acc, blog) => {
+		if (!acc.likes) {
+			return blog
+		}
+		if (acc.likes < blog.likes) {
+			return blog
+		}
+		return acc
+	}, [])
+}
+
 module.exports = {
   dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
