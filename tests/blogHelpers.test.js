6aab5195ac6703888b41f3a5ee2265b08c09d2d6
Asad Mahmood <asadmhmd1234@gmail.com>
2022-10-22 17:36:06 +0530

    Finish exercise 6

--- tests/blogHelpers.test.js
+++ tests/blogHelpers.test.js
@@ -1,126 +1,180 @@
-const listHelper = require('../utils/list_helper')
+const listHelper = require("../utils/list_helper");
+const { mostBlogs } = require("../utils/list_helper");
 
-test('dummy returns one', () => {
-	const blogs = []
+test("dummy returns one", () => {
+  const blogs = [];
 
-	const result = listHelper.dummy(blogs)
-	expect(result).toBe(1)
-})
+  const result = listHelper.dummy(blogs);
+  expect(result).toBe(1);
+});
 
-describe('total likes', () => {
+describe("total likes", () => {
   const listWithOneBlog = [
     {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
       likes: 5,
-      __v: 0
-    }
-  ]
+      __v: 0,
+    },
+  ];
 
-	const listOfManyBlogs = [
-		{
-			_id: ("6353888627d65a7de1f6946f"),
-			title: 'This is a title',
-			author: 'Asad Mahmood',
-			url: 'www.hoops.com',
-			likes: 10,
-			__v: 0
-		},
-		{
-			_id: ("6353b0e95f0d2db327197e4d"),
-			title: 'Another title',
-			author: 'Asad Mahmood',
-			url: 'www.hoops.com',
-			likes: 23,
-			__v: 0
-		},
-		{
-			_id: ("6353b1005f0d2db327197e4f"),
-			title: 'One more title',
-			author: 'Asad Mahmood',
-			url: 'www.hoops.com',
-			likes: 48,
-			__v: 0
-		}
-	]
+  const listOfManyBlogs = [
+    {
+      _id: "6353888627d65a7de1f6946f",
+      title: "This is a title",
+      author: "Asad Mahmood",
+      url: "www.hoops.com",
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: "6353b0e95f0d2db327197e4d",
+      title: "Another title",
+      author: "Asad Mahmood",
+      url: "www.hoops.com",
+      likes: 23,
+      __v: 0,
+    },
+    {
+      _id: "6353b1005f0d2db327197e4f",
+      title: "One more title",
+      author: "Asad Mahmood",
+      url: "www.hoops.com",
+      likes: 48,
+      __v: 0,
+    },
+  ];
 
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    expect(result).toBe(5)
-  })
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
 
-	test('when list has no blog, it equals to zero', () => {
-		const result = listHelper.totalLikes([])
-		expect(result).toBe(0)
-	})
+  test("when list has no blog, it equals to zero", () => {
+    const result = listHelper.totalLikes([]);
+    expect(result).toBe(0);
+  });
 
-	test('when list has many blogs, equals the sum of their likes', () => {
-		const result = listHelper.totalLikes(listOfManyBlogs)
-		expect(result).toBe(81)
-	})
-})
+  test("when list has many blogs, equals the sum of their likes", () => {
+    const result = listHelper.totalLikes(listOfManyBlogs);
+    expect(result).toBe(81);
+  });
+});
 
-describe('favourite blogpost', () => {
-	const listWithOneBlog = [
-		{
-			_id: '5a422aa71b54a676234d17f8',
-			title: 'Go To Statement Considered Harmful',
-			author: 'Edsger W. Dijkstra',
-			url: 'www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-			likes: 5,
-			__v: 0
-		}]
-	const listOfManyBlogs = [
-		{
-			_id: ("6353888627d65a7de1f6946f"),
-			title: 'This is a title',
-			author: 'Asad Mahmood',
-			url: 'www.hoops.com',
-			likes: 10,
-			__v: 0
-		},
-		{
-			_id: ("6353b0e95f0d2db327197e4d"),
-			title: 'Another title',
-			author: 'Asad Mahmood',
-			url: 'www.hoops.com',
-			likes: 23,
-			__v: 0
-		},
-		{
-			_id: ("6353b1005f0d2db327197e4f"),
-			title: 'one more title',
-			author: 'asad mahmood',
-			url: 'www.hoops.com',
-			likes: 48,
-			__v: 0
-		}
-	]
+describe("favourite blogpost", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+  const listOfManyBlogs = [
+    {
+      _id: "6353888627d65a7de1f6946f",
+      title: "This is a title",
+      author: "Asad Mahmood",
+      url: "www.hoops.com",
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: "6353b0e95f0d2db327197e4d",
+      title: "Another title",
+      author: "Asad Mahmood",
+      url: "www.hoops.com",
+      likes: 23,
+      __v: 0,
+    },
+    {
+      _id: "6353b1005f0d2db327197e4f",
+      title: "one more title",
+      author: "asad mahmood",
+      url: "www.hoops.com",
+      likes: 48,
+      __v: 0,
+    },
+  ];
 
-	test('if single blog post is sent, equals that post', () => {
-		const result = listHelper.favoriteBlog(listWithOneBlog)
-		expect(result).toEqual({
-			_id: '5a422aa71b54a676234d17f8',
-			title: 'Go To Statement Considered Harmful',
-			author: 'Edsger W. Dijkstra',
-			url: 'www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-			likes: 5,
-			__v: 0
-		})
-	})
+  test("if single blog post is sent, equals that post", () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    expect(result).toEqual({
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    });
+  });
 
-	test('if multiple blogs are sent, equals most liked post', () => {
-		const result = listHelper.favoriteBlog(listOfManyBlogs)
-		expect(result).toEqual({
-				_id: ("6353b1005f0d2db327197e4f"),
-				title: 'one more title',
-				author: 'asad mahmood',
-				url: 'www.hoops.com',
-				likes: 48,
-				__v: 0
-			}
-		)
-	})
-})
+  test("if multiple blogs are sent, equals most liked post", () => {
+    const result = listHelper.favoriteBlog(listOfManyBlogs);
+    expect(result).toEqual({
+      _id: "6353b1005f0d2db327197e4f",
+      title: "one more title",
+      author: "asad mahmood",
+      url: "www.hoops.com",
+      likes: 48,
+      __v: 0,
+    });
+  });
+});
+
+describe("author with most blogs", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+  const listOfManyBlogs = [
+    {
+      _id: "6353888627d65a7de1f6946f",
+      title: "This is a title",
+      author: "Asad Mahmood",
+      url: "www.hoops.com",
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: "6353b0e95f0d2db327197e4d",
+      title: "Another title",
+      author: "Asad Mahmood",
+      url: "www.hoops.com",
+      likes: 23,
+      __v: 0,
+    },
+    {
+      _id: "6353b1005f0d2db327197e4f",
+      title: "one more title",
+      author: "Another author",
+      url: "www.hoops.com",
+      likes: 48,
+      __v: 0,
+    },
+  ];
+  test("if no blogs are sent equals empty author with 0 posts", () => {
+    const result = mostBlogs([]);
+    expect(result).toEqual({ author: "", blogs: 0 });
+  });
+
+  test("if one blog is sent equals author on that post with one blog", () => {
+    const result = mostBlogs(listWithOneBlog);
+    expect(result).toEqual({ author: "Edsger W. Dijkstra", blogs: 1 });
+  });
+
+  test("if multiple blogs are sent equals author with most blogs", () => {
+    const result = mostBlogs(listOfManyBlogs);
+    expect(result).toEqual({ author: "Asad Mahmood", blogs: 2 });
+  });
+});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,25 +1,41 @@
+const _ = require("lodash");
 const dummy = (_) => {
-	return 1
-}
+  return 1;
+};
 
 const totalLikes = (blogs) => {
-	return blogs.reduce((acc, blog) => acc + blog.likes, 0)
-}
+  return blogs.reduce((acc, blog) => acc + blog.likes, 0);
+};
 
 const favoriteBlog = (blogs) => {
-	return blogs.reduce((acc, blog) => {
-		if (!acc.likes) {
-			return blog
-		}
-		if (acc.likes < blog.likes) {
-			return blog
-		}
-		return acc
-	}, [])
-}
+  return blogs.reduce((acc, blog) => {
+    if (!acc.likes) {
+      return blog;
+    }
+    if (acc.likes < blog.likes) {
+      return blog;
+    }
+    return acc;
+  }, []);
+};
+
+const mostBlogs = (blogs) => {
+  const authorCount = _.countBy(blogs, "author");
+  let result = { author: "", blogs: 0 };
+  for (const authorCountKey in authorCount) {
+    if (!result.blogs || result.blogs < authorCount[authorCountKey]) {
+      result = {
+        author: authorCountKey,
+        blogs: authorCount[authorCountKey],
+      };
+    }
+  }
+  return result;
+};
 
 module.exports = {
   dummy,
-	totalLikes,
-	favoriteBlog
-}
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+};
