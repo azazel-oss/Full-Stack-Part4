2bd4585cb256e26bfb4e54d9356949c05665f0f6
Asad Mahmood <asadmhmd1234@gmail.com>
2022-10-28 04:31:43 +0530

    Finish exercise 19

--- routes/BlogsRouter.js
+++ routes/BlogsRouter.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
 const Blog = require("../models/Blog");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const extractToken = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.split(" ")[1];
+  }
+  return null;
+};
 
 router.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { blogs: 0 });
@@ -7,7 +17,7 @@ router.get("/", async (request, response) => {
 });
 
 router.post("/", async (request, response) => {
-  const { title, url, likes } = request.body;
+  const { title, url, likes, author } = request.body;
   if (!title || !url) {
     return response.status(400).json({
       message: "not valid blog request",
@@ -17,9 +27,24 @@ router.post("/", async (request, response) => {
     request.body.likes = 0;
   }
 
-  const blog = new Blog(request.body);
-  const result = await blog.save();
-  response.status(201).json(result);
+  const token = extractToken(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({
+    title,
+    url,
+    likes,
+    author,
+    user: user.id,
+  });
+  const savedBlog = await blog.save();
+  user.blogs.push(savedBlog.id);
+  await user.save();
+  response.status(201).json(savedBlog);
 });
 
 router.put("/:id", async (request, response) => {
